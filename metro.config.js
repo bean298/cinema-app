5cead517c36189e9b6575bed2bf024dfe5ebc806
tuanbin298 <tuannase171419@fpt.edu.vn>
2025-08-20 22:13:36 +0700

    Setup config nativewwind

---
+++ metro.config.js
@@ -0,0 +1,8 @@
+// Nativewind config
+
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+
+module.exports = withNativeWind(config, { input: "./app/globals.css" });
